52da115767f66f54849a5be47bdf7b6a7c5422fe
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2025-02-04 15:23:45 +0200

    created hero (home)

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,15 @@
 export default function Home() {
-  return <span className="font-work-sans">Create</span>;
+  return (
+    <>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup <br />
+          Connect with entrepreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions
+        </p>
+      </section>
+    </>
+  );
 }
