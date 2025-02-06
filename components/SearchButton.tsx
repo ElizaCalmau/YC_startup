76787c1fa2b045828db50ecbfdf30aed7c35180d
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2025-02-06 18:02:43 +0200

    added search button component

---
+++ components/SearchButton.tsx
@@ -0,0 +1,7 @@
+import {Search} from "lucide-react";
+
+export const SearchButton = () => {
+    return (
+        <button type='submit' className='search-btn text-white'><Search className="size-5"/> </button>
+    );
+};
