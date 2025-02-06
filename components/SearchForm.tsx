828ad5fd6165f69b4e8988919eb197d120948f45
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2025-02-06 18:05:02 +0200

    added search button & updated import

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Form from "next/form";
-import {ResetSearchButton} from "@/app/components/ResetSearchButton";
+import {ResetSearchButton} from "@/components/ResetSearchButton";
+import {SearchButton} from "@/components/SearchButton";
 
 function SearchForm({query}: {query?: string} ) {
     return (
         <Form action="/" scroll={false} className="search-form">
             <input name="query" defaultValue="" className="search-input" placeholder="Search Startup" />
             {query && <ResetSearchButton />}
+            <SearchButton />
         </Form>
     )
 }
