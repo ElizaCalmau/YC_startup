0458d7f6d8449773fc588cdf16741d5c7f584808
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2025-02-13 19:23:11 +0200

    added SanityLive to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import localFont from "next/font/local";
 import 'easymde/dist/easymde.min.css'
+import {SanityLive} from "@/sanity/lib/live";
 
 const workSans = localFont({
   src: [
@@ -61,7 +62,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={workSans.variable}>{children}</body>
+      <body className={workSans.variable}>{children} <SanityLive /> </body>
     </html>
   );
 }
