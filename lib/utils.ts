48bfdcd80b8febf248e5257e2caeff747bce7e78
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2025-02-13 21:30:25 +0200

    created utility fn for date formatting

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,8 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatDate(date: Date) {
+  const formattedDate = new Date(date).toLocaleDateString('en-US', {month: 'long', day: "numeric", year: "numeric"})
+  return formattedDate;
+}
