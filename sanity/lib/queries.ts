c5d7f0be1269d76cf02b63417dbfcb3c8a5b1426
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2025-02-13 21:30:11 +0200

    updated query for Startup

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,3 +1,3 @@
 import {defineQuery} from "groq";
 
-export const STARTUP_QUERY = defineQuery(`*[_type == 'startup'] {_id, title, image, author -> {name, bio, id}}`)
+export const STARTUP_QUERY = defineQuery(`*[_type == 'startup'] {_createdAt, _id, title, category, views, image, description, author -> {name, bio, id, image}}`)
