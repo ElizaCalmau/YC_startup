5b0540241dfed18bb7554be0bfa29a488942d7dd
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2025-02-13 21:30:50 +0200

    rendered a Startup card component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,11 @@
 import SearchForm from "@/components/SearchForm";
 import {client} from "@/sanity/lib/client";
 import {STARTUP_QUERY} from "@/sanity/lib/queries";
+import {StartupCard} from "@/components/StartupCard";
 export default async function Home({searchParams}: {searchParams: Promise<{query?: string}>}) {
   const {query} = await searchParams;
-  const startup = await client.fetch(STARTUP_QUERY);
-  console.log('startup', startup);
+  const startups = await client.fetch(STARTUP_QUERY);
+  console.log('startup', startups);
   return (
     <>
       <section className="pink_container">
@@ -16,6 +17,16 @@ export default async function Home({searchParams}: {searchParams: Promise<{query
           Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions
         </p>
         <SearchForm query={query}/>
+      </section>
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for ${query}` : 'All Startups'}
+          <ul className="mt-7 card_grid">
+            {startups?.length > 0 ? (startups.map((startup: StartupCardType, index: number) => (
+                <StartupCard key={startup._id} startup={startup} />
+            ))) : <p>No startups found.</p>}
+          </ul>
+          </p>
       </section>
     </>
   );
