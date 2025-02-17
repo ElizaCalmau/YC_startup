2952a7bcd5c4712c033c0bdb5429c68bd67a83c2
Yelyzaveta Lysenko <elizabethcalmau@gmail.com>
2025-02-17 17:24:48 +0200

    created new type for card based on Sanity type

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -1,10 +1,13 @@
 import {formatDate} from "@/lib/utils";
-
 import Link from "next/link";
 import Image from "next/image";
 import {Button} from '@/components/ui/button'
+import {Startup, Author} from "@/sanity/types";
+
+export type StartupTypeCard = Omit<Startup, "author"> & {author?: Author};
+
 export const StartupCard = ({startup}: {startup: StartupTypeCard}) => {
-    const { _createdAt, views, author: {_id: authorId, name}, title, category, _id, image, description } = startup;
+    const { _createdAt, views, author, title, category, _id, image, description, _type } = startup;
     const date = formatDate(_createdAt);
     console.log(date, "date")
     return (
@@ -18,14 +21,14 @@ export const StartupCard = ({startup}: {startup: StartupTypeCard}) => {
             </div>
             <div className="flex-between mt-5 gap-5">
                 <div className="flex-1">
-                    <Link href={`/users/${authorId}`}>
-                        <p className="text-16-medium line-clamp-1"> {name}</p>
+                    <Link href={`/users/${author?._id}`}>
+                        <p className="text-16-medium line-clamp-1"> {author?.name}</p>
                     </Link>
                     <Link href={`startup/${_id}`}>
                         <h3 className="text-26-semibold line-clamp-1"> </h3>
                     </Link>
                 </div>
-                <Link href={`/users/${authorId}`} >
+                <Link href={`/users/${author?._id}`} >
                     <Image src={image} alt="startup image" width={48} height={48} className="rounded-full" />
                 </Link>
             </div>
@@ -36,7 +39,7 @@ export const StartupCard = ({startup}: {startup: StartupTypeCard}) => {
                 <img src={image} alt="startup image" className="startup-card_img" />
             </Link>
             <div className="flex-between gap-3 mt-3">
-                <Link href={`/?query=/${category.toLowerCase()}`}>
+                <Link href={`/?query=/${category?.toLowerCase()}`}>
                     <p className="text-16-medium">{category} </p>
                 </Link>
                 <Button className="startup-card_btn asChild">
